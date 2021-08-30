250b2aee4b44c6b1179fd547b640b23406db1596
Erick <erickfco1999m@gmail.com>
2021-08-30 10:48:12 -0500

    Integracion de data y formulario

--- django_redBeach_project/red_beach_advisors/views.py
+++ django_redBeach_project/red_beach_advisors/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
-from .models import Register
+from .models import Answers, Register, Questions
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import signUpForm
@@ -9,7 +9,41 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def index(request):
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+        
     if request.method == 'POST':
+        saveDataVeteran = Answers(txtanswer= request.POST['discharge_type'], id_question=Questions.objects.get(id_question=1), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['amount_retire'], id_question=Questions.objects.get(id_question=2), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['location'], id_question=Questions.objects.get(id_question=3), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['level_education'], id_question=Questions.objects.get(id_question=4), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['career'], id_question=Questions.objects.get(id_question=5), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['years_career'], id_question=Questions.objects.get(id_question=6), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['job_role'], id_question=Questions.objects.get(id_question=7), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['married'], id_question=Questions.objects.get(id_question=8), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['members_family'], id_question=Questions.objects.get(id_question=9), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['work'], id_question=Questions.objects.get(id_question=10), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['select_place'], id_question=Questions.objects.get(id_question=11), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['family'], id_question=Questions.objects.get(id_question=12), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['cities'], id_question=Questions.objects.get(id_question=13), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['sizeC'], id_question=Questions.objects.get(id_question=14), email_register=username)
+        saveDataVeteran.save()
+        saveDataVeteran = Answers(txtanswer= request.POST['buy_home'], id_question=Questions.objects.get(id_question=15), email_register=username)
+        saveDataVeteran.save()
         return redirect('results')
     else:
         return render(request, 'questions.html')
